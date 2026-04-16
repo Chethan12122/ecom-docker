1e199c43a6fa36877ff1f3c97541549918f4ebb4
Chethan12122 <chethan@aikyamsports.com>
2026-04-16 16:40:24 +0530

    brake  failure test

--- ecom_django/ecommerce/products/tests.py
+++ ecom_django/ecommerce/products/tests.py
@@ -1,7 +1,8 @@
-
-# Create your tests here.
-# tests.py
 from django.test import TestCase
 
-def test_example(self):
-    self.assertEqual(1 + 1, 3)   # wrong
+
+#create test here
+class SampleTest(TestCase):
+
+    def test_example(self):
+        self.assertEqual(1 + 1, 3)   # ❌ this will FAIL
